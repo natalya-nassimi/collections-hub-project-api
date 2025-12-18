19ea20dbc39e60ce26bfe9e6e15d6cadedc1556c
Natalya Nassimi <natalya1ahronee@gmail.com>
2025-12-18 20:29:01 +0000

    bug fix

--- collection/serializers/common.py
+++ collection/serializers/common.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 from ..models import Collection
 
 class CollectionSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
     class Meta:
         model = Collection
-        fields = ('id', 'title', 'description', 'image', 'created_at', 'updated_at', 'user')
+        fields = ('id', 'title', 'description', 'image', 'created_at', 'updated_at', 'user', 'username')
         read_only_fields = ('id', 'created_at', 'updated_at', 'user')
